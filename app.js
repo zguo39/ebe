60e076e752475963b8d368e9a226276d056f0d0b
zixiao guo <zguo39@student.monash.edu>
2021-04-30 03:09:30 +1000

    typeupdate

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 
 // create server and listen port
 const port = process.env.PORT || 8888;
+// const port = process.env.PORT
 
 app.listen(port, function() {
 	console.log(`USING: ${port}`)
@@ -44,6 +45,76 @@ app.use((req, res, next) => {
 	next();
 });
 
+// api get video
+app.get('/api/type', async (req, res) => {
+	let {
+		type,
+	} = req.query
+	let data
+	
+	let sqlquery = "Select `Core Areas`,`Exercise Name`,Description,`Youtube Link`,`Exercise Precautions` from exercise where `Core Areas` = '" + type +"';"
+	try {
+		data = await new Promise((resolve, reject) => {
+			connect.query(sqlquery, function(err, result) {
+				if (err) {
+					reject(err)
+				} else {
+					resolve(result)
+				}
+			})
+		})
+		responseInfo(res, {
+			data
+		})
+	} catch (e) {
+		//TODO handle the exception
+		responseInfo(res, {
+			code: 1,
+			codeText: e
+		})
+	}	
+})
+
+// condition
+app.get('/api/condition', async (req, res) => {
+	let {
+		condition,
+	} = req.query
+	let data
+	condition = condition.split(',')
+
+	let sqlquery = "Select `Core Areas`,`Exercise Name`,Description,`Youtube Link`,`Exercise Precautions` from exercise where `Health Condition` = '"
+	
+	for(let i = 0; i < condition.length; i++) {
+		if(i == 0) {
+			sqlquery += `${condition[i]}'`;
+		}else {
+			sqlquery += " or `Health Condition` = '" + condition[i] + "'"
+		}
+	}
+	
+	try {
+		data = await new Promise((resolve, reject) => {
+			connect.query(sqlquery, function(err, result) {
+				if (err) {
+					reject(err)
+				} else {
+					resolve(result)
+				}
+			})
+		})
+		responseInfo(res, {
+			data
+		})
+	} catch (e) {
+		//TODO handle the exception
+		responseInfo(res, {
+			code: 1,
+			codeText: e
+		})
+	}	
+})
+
 // api get request,
 app.get('/api/daily', async (req, res) => {
 	let {
@@ -52,8 +123,6 @@ app.get('/api/daily', async (req, res) => {
 	} = req.query
 	let data
 	
-	// test query
-	// let sqlquery = "select * from master_nutrient where Gender='" + gender + "' and Age=" + age +""
 	let sqlquery = "select `Protein in g`, `Vitamin A in ug`, `Vitamin C in mg`, `Calcium in mg`, `Folate in ug`, `Potassium in mg`, `Iron in mg`, `Zinc in mg`, `Vitamin B6 in mg`, `Vitamin B12 in ug`, `Magnesium in mg` from master_nutrient where Gender='" + gender + "' and Age=" + age +""
 
 	try {
@@ -75,9 +144,7 @@ app.get('/api/daily', async (req, res) => {
 			code: 1,
 			codeText: e
 		})
-	}
-	// let sqlquery = "select `Protein in g`, `Vitamin A retinol equivalents in ug`, `Vitamin C in mg`, `Calcium (Ca) in mg`, `Total Fat in g`, `Total sugars in g` from master_food where `Food Name` Like '%" + item + "%'"
-	
+	}	
 })
 
 // get distinct category list
